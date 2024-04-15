1fa9094782ca1c7b435b55eb48db3fa57c86ea5a
Abhijeet Kumar <abhijeetthakur7080@gmail.com>
2024-04-15 18:25:17 +0530

    gh-pages updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 import { AuthContextProvider } from "./redux/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <Router>
     <AuthContextProvider>
       <Provider store={store}>
         <App />
         <Toaster />
       </Provider>
     </AuthContextProvider>
-  </BrowserRouter>
+  </Router>
 );
